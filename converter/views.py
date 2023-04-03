9dae41fd2ba1ac81bb5af6f9850b03d089694166
bunglebi4 <bunglebi4@mail.ru>
2023-04-03 20:25:56 +0300

    pre-final version without docker

--- converter/views.py
+++ converter/views.py
@@ -1,16 +1,12 @@
+from json import loads
 import json
 from lxml import etree
-
 import xmltodict
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-from json import dumps, loads
 from dicttoxml import dicttoxml
 from django.http import HttpResponse
-
 from testsite import settings
-
-
 from django.conf import settings
 
 
@@ -29,15 +25,11 @@ def get_xsd_string(path):
 @csrf_exempt
 def to_xml(request):
     if request.method == 'POST':
-
         json_data = loads(request.body)
-
         data_dict = json_data if isinstance(json_data, dict) else json.loads(json_data.decode('utf-8'))
-
         xml_data = dicttoxml(data_dict, custom_root='EntrantChoice')
         if isinstance(xml_data, str):
             xml_data = xml_data.encode('utf-8')
-
         if getattr(settings, 'VALIDATE_WITH_XSD', False):
             xsd_string = get_xsd_string("converter/validation/Add_Entrant_List.xsd")
             if isinstance(xsd_string, str):
@@ -45,7 +37,6 @@ def to_xml(request):
             is_valid = validate_xml(xml_data, xsd_string)
             if not is_valid:
                 return JsonResponse({'error': 'XML validation failed'})
-
         return HttpResponse(xml_data, content_type='application/xml')
     else:
         return JsonResponse({'error': 'Invalid request method'})
@@ -55,11 +46,8 @@ def to_xml(request):
 def from_xml(request):
 
     if request.method == 'POST':
-        # XML передается в сериализованном виде в связи с этим ее надо декодировать в utf
         xml_string = request.body
-        # переводим ее в dict
         xml_dict = xmltodict.parse(xml_string)
-
         if getattr(settings, 'VALIDATE_WITH_XSD', False):
             xsd_string = get_xsd_string("converter/validation/Get_Entrant_List.xsd")
             if isinstance(xsd_string, str):
@@ -67,6 +55,4 @@ def from_xml(request):
             is_valid = validate_xml(xml_string, xsd_string)
             if not is_valid:
                 return JsonResponse({'error': 'XML validation failed'})
-
-        # indent существует строго для красоты и не несет смысла
         return JsonResponse(xml_dict, json_dumps_params={'indent': 2})
